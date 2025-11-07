c072d17bfd14f30c7bc31298d1a9598dfb059793
grupopi25 <grupopiii27@gmail.com>
2025-11-06 21:02:55 -0300

    criando as rotas de usuários

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+public function login(Request $request){
+
+$request->only(['email','password']);
+$user = User::where('email',$request->email)->fisrt();
+
+if(!$user || Hash::check($request->password, $user->password)){ // CHECA SE O USUÁRIO EXISTE
+    return response()->json(
+        [
+            'status' => 'error',
+            'message' => 'Usuário não cadastrado!'
+        ],Response::HTTP_UNAUTHORIZED
+    );
+}
+$token = $user->createToken($request->email)->plainTextToken;  // CRIA UM TOKEN PARA SALVAR UM USUARIO 
+ 
+return response()->json([
+
+    'status' => 'sucesso',
+    'user' => $user,
+    'token' => $token    
+],Response::HTTP_OK);
+
+
+}
+public function logout(Request $request){ // FAZ O USUÁRIO DESLOGAR DA CONTA 
+
+  
+    $user = $request -> user();
+    $user ->tokens() ->delete();
+
+    return response()->json(
+
+         [
+            'status' => 'sucesso',
+            'message' => 'Usuário saiu com Sucesso'
+        ],Response::HTTP_OK  
+    );
+}
+
+public function registro(Request $request){  // FAZ A VALIDAÇÃO DOS CAMPOS
+$request->validate([
+    'name' => 'required',
+    'email' => 'required|email',
+    'password' => 'required|min:4',
+    
+    $user = User::create(                // CRIA O USUAROP
+        [
+               'name' => $request->name, 
+               'email' => $request->email, 
+               'password' => Hash::make($request->password), 
+        ],response::HTTP_CREATED
+    )
+]);
+}
+
+
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,9 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::get('/adm',function(Request $request){
-    return response()->json([
-        ['id'=> 1, 'nome' => 'Eduardo', 'valor' => 0],
-        ['id'=> 2, 'nome' => 'Luiza'  , 'valor' => 250],
-        ['id'=> 3, 'nome' => 'Paula'  , 'valor' => 100],
-        
-        
-    ]);;
-});
+Route::post('/login',[UserController::class,'login']);
+Route::post('/registro',[UserController::class,'registro']);
+Route::middleware('auth:sanctum')->post('logout',[UserController::class,'logout']);
